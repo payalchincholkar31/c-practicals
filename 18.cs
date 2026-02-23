a94d2f4c9ad8d9fbc6a3174408291900d6f4861d
payalchincholkar31 <payalchincholkar53@gmail.com>
2026-02-23 14:35:07 +0530

    practical1

---
+++ 18.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace MultiplicationTableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nMultiplication Table of " + number + ":\n");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(number + " x " + i + " = " + (number * i));
+            }
+
+            Console.ReadLine(); // Keeps console open
+        }
+    }
+}
