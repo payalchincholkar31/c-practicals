20d3a71ea667c928ee13d4a9edd411ace8bf0059
payalchincholkar31 <payalchincholkar53@gmail.com>
2026-02-23 19:14:27 +0530

    practical1

---
+++ 32.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Demonstrating while loop
+        Console.WriteLine("Numbers from 1 to 10:");
+
+        int i = 1;
+
+        while (i <= 10)
+        {
+            Console.WriteLine(i);
+            i++;   // increment
+        }
+
+        Console.ReadLine();
+    }
+}
