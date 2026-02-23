514279671bcd851398a568dc84f9a8c10047e2e1
payalchincholkar31 <payalchincholkar53@gmail.com>
2026-02-23 19:28:33 +0530

    practical

---
+++ 41.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a year: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine(year + " is a Leap Year.");
+        }
+        else
+        {
+            Console.WriteLine(year + " is not a Leap Year.");
+        }
+
+        Console.ReadLine();
+    }
+}
