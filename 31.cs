77563c1ff64f4058bd6b899999e5d465ad45cea0
payalchincholkar31 <payalchincholkar53@gmail.com>
2026-02-23 19:12:17 +0530

    practical1

---
+++ 31.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Demonstrating for loop
+        Console.WriteLine("Numbers from 1 to 10:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.ReadLine();
+    }
+}
